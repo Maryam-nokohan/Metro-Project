bcb978cbea8d67389c6e9c96678f77b7aa6e1dc0
Maryam-Nokohan <maryam.nokohan@gmail.com>
2026-08-05 01:11:10 +0200

    feat : implemented bellman algorithm

---
+++ algorithms/bellman_ford.py
@@ -0,0 +1,71 @@
+from typing import Dict, List, Optional, Tuple
+
+from models.graph import Graph
+
+
+def bellman_ford(
+    graph: Graph,
+    start: str,
+    criterion: str = "weight",
+):
+    """
+    Returns:
+        dist
+        parent
+        negative_cycle
+    """
+
+    dist = {
+        station: float("inf")
+        for station in graph.station_ids()
+    }
+
+    parent = {}
+
+    dist[start] = 0
+
+    n = graph.num_stations()
+
+    for _ in range(n - 1):
+
+        updated = False
+
+        for edge in graph.edges():
+
+            u = edge.source
+            v = edge.destination
+
+            w = edge.get_weight(criterion)
+
+            if dist[u] == float("inf"):
+                continue
+
+            if dist[u] + w < dist[v]:
+
+                dist[v] = dist[u] + w
+
+                parent[v] = u
+
+                updated = True
+
+        if not updated:
+            break
+
+    negative_cycle = []
+
+    for edge in graph.edges():
+
+        u = edge.source
+        v = edge.destination
+
+        w = edge.get_weight(criterion)
+
+        if dist[u] != float("inf"):
+
+            if dist[u] + w < dist[v]:
+
+                negative_cycle.append(
+                    (u, v)
+                )
+
+    return dist, parent, negative_cycle
